e4abd806aaf067b3383122d3aff4f159f04410ad
lilyH <hlily871108@gmail.com>
2014-10-11 17:16:20 +0800

    增加对ghost click的处理
    增加对ghost click的处理

--- zepto-touch.js
+++ zepto-touch.js
@@ -12,9 +12,12 @@
     window.CustomEvent = CustomEvent;
 })();
 
+/**
+ * 基于zepto的touch事件，事件只会绑定在特定的元素上，不会绑定到document上
+ */
 (function($) {
-    
     'use strict';
+    var t,x,y;
     var _Touch = function(el){
         function createCustomEvent(touchName){
             var evt;
@@ -38,19 +41,23 @@
             startY : 0, //starting y coordinate
             hasTouchEventOccured : false, //flag touch event
 
-            start : function (e) {
+            start : function (e) { 
+                
                 if (e.type === 'touchstart') {
                     this.hasTouchEventOccured = true;
                 }
                 this.moved = false;
                 this.startX = e.type === 'touchstart' ? e.touches[0].clientX : e.clientX;
                 this.startY = e.type === 'touchstart' ? e.touches[0].clientY : e.clientY;
+                x = e.touches[0].clientX;
+                y = e.touches[0].clientY;
             },
 
             move : function (e) {
-                //if finger moves more than 10px flag to cancel
-                // 移动大于10px的时候认为移动了
-                if (Math.abs(e.touches[0].clientX - this.startX) > 10 || Math.abs(e.touches[0].clientY - this.startY) > 10) {
+                //if finger moves more than 30px flag to cancel
+                // 移动大于30px的时候认为移动了
+            
+                if (Math.abs(e.touches[0].clientX - this.startX) > 30 || Math.abs(e.touches[0].clientY - this.startY) > 10) {
                     if(e.touches[0].clientX - this.startX > 0){
                         this.moveDirection = 'right';
                     }
@@ -58,18 +65,22 @@
                         this.moveDirection = 'left';
                     }
                     this.moved = true;
+                   
                 }
+                x = e.touches[0].clientX;
+                y = e.touches[0].clientY;
             },
-
             end : function (e) {
+               
                 var evt;
                 if (this.hasTouchEventOccured) {
+
                     e.preventDefault();
                     e.stopPropagation();
                     this.hasTouchEventOccured = false;
                     //return;
                 }
-
+              
                 if (!this.moved) {
                     evt = createCustomEvent('tap');
                 }else{
@@ -84,8 +95,13 @@
                     // in which case we want to prevent clicks from firing.
                     e.preventDefault();
                 }
+                //不支持preventDefault的机型，才会去click里面阻止click的原生行为
+                if(!e.defaultPrevented){
+                    t = new Date();
+                }
             },
             cancel : function (e) {
+              
                 this.hasTouchEventOccured = false;
                 this.moved = false;
                 this.startX = 0;
@@ -104,29 +120,32 @@
                     case 'touchstart': this.start(e); break;
                     case 'touchmove': this.move(e); break;
                     case 'touchend': this.end(e); break;
-                    case 'touchcancel': this.cancel(e); break;
+                    case 'touchcancel': this.end(e); break;// touchcancel == touchend
                 }
             }
         };
+
         return touch;
          
     };
     var Touch = function(el){
         var func = _Touch(el);
+
         el.addEventListener('touchstart', func, false);
         el.addEventListener('touchmove', func, false);
         el.addEventListener('touchend', func, false);
         el.addEventListener('touchcancel', func, false);
+
     };
     var unTouch = function(el){
         _Touch(el).destroy();
     };
     var oldBind = $.fn.on,
         oldUnBind = $.fn.off,
-        onArray = [];//对于相同的el只绑定一次
+        onArray = [];
     $.fn.on = function( evt ){
-
-        if( /(^| )(tap|swipeleft|swiperight)( |$)/.test( evt ) ){
+        
+        if( /(^| )(tap|swipeleft|swiperight)( |$)/.test( evt ) ){ 
             if(onArray.length == 0){
                 for(var i=0;i<this.length;i++){
                     onArray.push(this[i]);
@@ -153,11 +172,20 @@
     };
     $.fn.off = function( evt ){
         if( /(^| )(tap|swipeleft|swiperight)( |$)/.test( evt ) ){
-            for(var i=0; i<this.length; i++){
-                unTouch( this[i] );
+            for(var i=0 ;i<this.length;i++){
+               unTouch( this[i] );
             }
         }
         return oldUnBind.apply( this, arguments );
     };
- 
-})(Zepto);
+    //去掉ghost click 详细见http://ariatemplates.com/blog/2014/05/ghost-clicks-in-mobile-browsers/
+    window.addEventListener('click', function (e) {
+        var time_threshold = 500,
+            space_threshold = 100;
+     
+        if (new Date() - t <= time_threshold && Math.abs(e.clientX - x) <= space_threshold && Math.abs(e.clientY - y) <= space_threshold) {
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    }, true);
+}(Zepto));
